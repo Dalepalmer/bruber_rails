92b2b6922f143d48e000f4bd4b0d4c783f8ce73f
Dale <dap38@zips.uakron.edu>
2015-04-07 14:28:28 -0700

    added devise for multiple user sign ins

---
+++ app/assets/javascripts/bike_repair.js
@@ -0,0 +1,34 @@
+
+/**
+ * @ngdoc overview
+ * @name bikeRepair
+ * @description
+ * # bikeRepair
+ *
+ * Main module of the application.
+ */
+
+angular
+  .module('bikeRepair', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ng-token-auth'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/models/mechanic.rb
@@ -0,0 +1,6 @@
+class Mechanic < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
