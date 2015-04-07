7a082a7a45fc47b81199e094a4857f46ae3df8df
Ben <ben.s.pritchard@gmail.com>
2015-04-07 12:13:45 -0700

    Add additional model specs for validations.

--- spec/models/shoulda_matchers_spec.rb
+++ spec/models/shoulda_matchers_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 describe Customer do
   it { should have_many :bicycles }
   it { should have_many :tickets }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
+  it { should validate_confirmation_of :password }
+  it { should validate_presence_of :cell }
+  it { should validate_presence_of :payment }
 end
 
 describe Bicycle do
@@ -12,6 +17,11 @@ end
 
 describe Mechanic do
   it { should have_many :tickets }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :email }
+  it { should validate_confirmation_of :password }
+  it { should validate_presence_of :cell }
+  it { should validate_presence_of :payment }
 end
 
 describe Ticket do
@@ -19,6 +29,7 @@ describe Ticket do
   it { should belong_to :mechanic }
   it { should belong_to :problem }
   it { should belong_to :customer }
+  it { should validate_presence_of :status }
 end
 
 describe Problem do
