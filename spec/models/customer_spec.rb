c4153560e31400891ce6b703dad9005e718fc3dc
Revelation Interns <revelationinterns@Zer0HZ.local>
2015-05-29 17:23:17 -0700

    Test Associations for Customer Class

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 describe Customer do
 
+  it { should have_many :bicycles }
+  it { should have_many :tickets }
+
+  it { should have_many(:messages).through(:tickets) }
+
 end
