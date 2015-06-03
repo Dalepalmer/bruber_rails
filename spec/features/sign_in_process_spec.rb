f9a0822a4672a9dd086c53e0cb9f55565a06b887
Alex Beeken <aeb242@gmail.com>
2015-06-02 20:52:41 -0700

    Test For Logging In Process Correctly

--- spec/features/sign_in_process_spec.rb
+++ spec/features/sign_in_process_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
-describe "The Create Repair Ticket Process" do
+describe "The Sign In Process" do
     before(:each) do
         user = FactoryGirl.create(:user)
         sign_in(user)
     end
 
     it "Allows a user to sign in." do
-
+        expect(page).to have_content "Signed in"
     end
 end
